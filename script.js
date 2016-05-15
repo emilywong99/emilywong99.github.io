ce393bcf87feb220232a18376e77fec017673902
Emily Wong <emilywong99@gmail.com>
2016-05-15 13:14:26 -0700

    Main Update
    Has all of the updates recently set

---
+++ script.js
@@ -0,0 +1,11 @@
+//for the footer
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " EmilyWongInc. All rights reserved.</p>";
+
+//adds the navigation menu
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='emilyweb.html'>About Me</a></li>" +
+"<li><a href='emilyprojects.html'>Portfolio</a></li>" +
+"<li><a href='emilycontact.html'>Contact Information</a></li>" +
+"</ul>";
